6fa2bbfba458ea82390349654ce779eb473c78d3
Matt Thomson <matt.thomson@mendeley.com>
2014-11-11 12:48:15 +0000

    Format HDFS.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@ include_recipe 'java::default'
 include_recipe 'hadoop::default'
 include_recipe 'hadoop::hadoop_hdfs_namenode'
 include_recipe 'hadoop::hadoop_hdfs_datanode'
+
+ruby_block 'format-hdfs' do
+  block do
+    resources('execute[hdfs-namenode-format]').run_action(:run)
+    node.set['hdfs-formatted'] = true
+    node.save
+  end
+  not_if { node.attribute?('hdfs-formatted') }
+end
