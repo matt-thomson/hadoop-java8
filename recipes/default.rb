849f0244030ad9724bb5a811ef0f0d680ac4ee1d
Matt Thomson <matt.thomson@mendeley.com>
2014-11-11 15:43:46 +0000

    Fix up HDFS permissions.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,24 +7,46 @@ include_recipe 'hadoop::hadoop_hdfs_datanode'
 ruby_block 'format-hdfs' do
   block do
     resources('execute[hdfs-namenode-format]').run_action(:run)
+  end
+  notifies :create, 'ruby_block[set-hdfs-formatted-flag]', :immediately
+  not_if { node.attribute?('hdfs-formatted') }
+end
+
+ruby_block 'set-hdfs-formatted-flag' do
+  block do
     node.set['hdfs-formatted'] = true
     node.save
   end
-  not_if { node.attribute?('hdfs-formatted') }
+  action :nothing
 end
 
 ruby_block 'start-namenode' do
   block do
-    %w(enable start).each do |action|
-      resources('service[hadoop-hdfs-namenode]').run_action(action.to_sym)
+    [:enable, :start].each do |action|
+      resources('service[hadoop-hdfs-namenode]').run_action(action)
     end
   end
 end
 
 ruby_block 'start-datanode' do
   block do
-    %w(enable start).each do |action|
-      resources('service[hadoop-hdfs-datanode]').run_action(action.to_sym)
+    [:enable, :start].each do |action|
+      resources('service[hadoop-hdfs-datanode]').run_action(action)
     end
   end
+end
+
+execute 'set-hdfs-permissions' do
+  user 'hdfs'
+  command 'hdfs dfs -chmod 777 /'
+  notifies :create, 'ruby_block[set-hdfs-permissions-flag]', :immediately
+  not_if { node.attribute?('hdfs-permissions-set') }
+end
+
+ruby_block 'set-hdfs-permissions-flag' do
+  block do
+    node.set['hdfs-permissions-set'] = true
+    node.save
+  end
+  action :nothing
 end
--- test/integration/default/serverspec/hadoop_spec.rb
+++ test/integration/default/serverspec/hadoop_spec.rb
@@ -10,3 +10,10 @@ end
 describe command('hdfs dfs -ls /') do
     its(:exit_status) { should eq 0 }
 end
+
+describe command('hdfs dfs -cat test') do
+  let(:pre_command) { 'echo abc123 | hdfs dfs -put -f - test' }
+
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should eq "abc123\n" }
+end
