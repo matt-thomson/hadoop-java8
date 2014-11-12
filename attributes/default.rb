59eb01c902f1fbd5b1a3846eb0b4f15bec940b5a
Matt Thomson <matt.thomson@mendeley.com>
2014-11-12 12:21:21 +0000

    Use CDH Hadoop (which only supports LTS Ubuntu).

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,6 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 default['java']['set_etc_environment'] = true
+
+default['hadoop']['distribution'] = 'cdh'
+default['hadoop']['distribution_version'] = '5'
