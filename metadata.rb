4d51cbcd54d25014c4b5ae98c73e1cadeef424b5
Matt Thomson <mattjohnthomson@gmail.com>
2015-01-03 17:35:10 +0000

    Release v0.2.0.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mattjohnthomson@gmail.com'
 license          'MIT'
 description      'Installs/Configures hadoop-java8'
 long_description 'Installs/Configures hadoop-java8'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'java'
 depends 'hadoop'
